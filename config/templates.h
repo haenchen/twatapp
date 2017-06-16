a2a8dcacd7c7026279711e071aea69ab12db99fd
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2017-06-16 13:40:24 +0200

    Added forgotten files

---
+++ config/templates.h
@@ -0,0 +1,12 @@
+/* 
+ * format:
+ * .speed      (int),
+ * .frames     (int),
+ * .width      (int),
+ * .height     (int),
+ * .path       (string),
+ * .directions (int[4])
+ * */
+{.speed = 8, .frames = 2, .width = 32, .height = 32, .path = "res/player_sprite.bmp", .directions = {0, 1, 2, 3} }, // player
+{.speed = 8, .frames = 1, .width = 13, .height = 13, .path = "res/bullet.bmp", .directions = {0} } // bullet
+
